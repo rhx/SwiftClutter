cce2eb04544d1980752a3c7733dfebc18709e6c0
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-10 11:09:19 +1000

    Adapt to updated directory structure

--- Package.swift
+++ Package.swift
@@ -6,13 +6,14 @@ let package = Package(
     name: "Clutter",
     products: [ .library(name: "Clutter", targets: ["Clutter"]) ],
     dependencies: [
-        .package(name: "Cairo", url: "https://github.com/rhx/SwiftCairo.git", .branch("main")),
-        .package(name: "CoglPango", url: "https://github.com/rhx/SwiftCoglPango.git", .branch("main")),
-        .package(name: "Atk", url: "https://github.com/rhx/SwiftAtk.git", .branch("main")),
-        .package(name: "GIO", url: "https://github.com/rhx/SwiftGIO.git", .branch("main")),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
+        .package(name: "Cairo", url: "https://github.com/rhx/SwiftCairo.git", .branch("development")),
+        .package(name: "CoglPango", url: "https://github.com/rhx/SwiftCoglPango.git", .branch("development")),
+        .package(name: "Atk", url: "https://github.com/rhx/SwiftAtk.git", .branch("development")),
+        .package(name: "GIO", url: "https://github.com/rhx/SwiftGIO.git", .branch("development")),
     ],
     targets: [
-	.systemLibrary(name: "CClutter", pkgConfig: "clutter-1.0 cogl-gl-1.0 cogl-path-1.0 glib-2.0 gio-unix-2.0",
+	.systemLibrary(name: "CClutter", pkgConfig: "clutter-1.0",
 	    providers: [
 		.brew(["clutter", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libclutter-1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
